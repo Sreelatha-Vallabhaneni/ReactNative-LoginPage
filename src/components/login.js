01a5aec7aa2d7fcf940398dc75b75d634727cf85
Sreelatha Vallabhaneni <sreelatha.vallabhaneni@gmail.com>
2020-08-11 13:01:44 +0200

    added logo

--- src/components/login.js
+++ src/components/login.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { StyleSheet, Text, View, StatusBar } from "react-native";
+import { StyleSheet, Text, View, StatusBar, Image } from "react-native";
 import LoginForm from './form';
+//import logo from '../../assets/'
 
 
 function LoginPage() {
   return (
     <View style={styles.container}>
       <StatusBar barStyle="light-content" />
-      <Text style={{color: "navy", fontWeight: "700"}} >Github Login Page!</Text>
+      <Image source={require("../../assets/reactnative-logo.png")} style={styles.logo} />
+      <Text style={{color: "navy", fontWeight: "700"}} >React Native Login Page!</Text>
       <View>
         <LoginForm />
       </View>
@@ -23,6 +25,12 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  logo: {
+    marginVertical: 20,
+    width: 100,
+    height: 100,
+    borderRadius: 8
+  }
 });
 
 export default LoginPage;
